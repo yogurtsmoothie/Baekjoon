740ada5c75d10310da9036078f5c9c127d3bbbe9
yogurtsmoothie <green7170@naver.com>
2021-09-22 23:34:48 +0900

    O

---
+++ C99/10172/10172.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%s\n", "|\\_/|");
+	printf("%s\n", "|q p|   /}");
+	printf("%s\n", "( 0 )\"\"\"\\");
+	printf("%s\n", "|\"^\"`    |");
+	printf("%s\n", "||_/=\\\\__|");
+}
