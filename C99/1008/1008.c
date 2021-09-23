5dfb93f6f703752c323968a4200b174e174c1803
yogurtsmoothie <green7170@naver.com>
2021-09-23 23:55:21 +0900

    X

---
+++ C99/1008/1008.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	float A, B;
+	float result;
+
+	A < 0;
+	B < 10;
+
+	scanf_s("%f", &A);
+	scanf_s("%f", &B);
+	
+	result = A / B;
+
+	printf("%20.15f", result);
+}
