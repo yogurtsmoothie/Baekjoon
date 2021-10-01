a15a4d30143ad288793ce97f156091c565358069
yogurtsmoothie <green7170@naver.com>
2021-10-01 23:25:31 +0900

    O

---
+++ C99/10869/10869.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int A, B;
+	A >= 1;
+	B <= 10000;
+
+
+	scanf_s("%d", &A);
+	scanf_s("%d", &B);
+	
+	printf("%d \n", A + B);
+	printf("%d \n", A - B);
+	printf("%d \n", A * B);
+	printf("%d \n", A / B);
+	printf("%d \n", A % B);
+}
